451138d53a38b0c69e9fedf61371ca4ae97061ee
Caio Diego <caiodsj96@gmail.com>
2023-10-05 14:36:12 -0300

    add component admin

--- evaluator/src/app/app.module.ts
+++ evaluator/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ProjectComponent } from './components/admin/project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AdminComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
