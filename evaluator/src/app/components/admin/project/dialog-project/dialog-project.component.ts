4a6b6b151fcd9e4e4b1da5d95f7e28d2e1a66417
Caio Diego <caiodsj96@gmail.com>
2023-10-07 16:18:31 -0300

    add dialog-form project

--- evaluator/src/app/components/admin/project/dialog-project/dialog-project.component.ts
+++ evaluator/src/app/components/admin/project/dialog-project/dialog-project.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -9,4 +9,26 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class DialogProjectComponent {
 
+  formProject: FormGroup;
+
+  listaCriterios: any[] = ['criterio1', 'criterio2', 'criterio3', 'criterio4', 'criterio5'];
+
+  constructor(public _fb: FormBuilder, private _dialogRef: MatDialogRef<DialogProjectComponent>) { 
+    this.formProject = _fb.group({
+      titulo: '',
+    });
+
+    // Adicione controles para seleção e peso de cada critério dinamicamente
+    this.listaCriterios.forEach((criterio, index) => {
+      this.formProject.addControl('selecionarCriterio' + index, new FormControl(false)); // Checkbox
+      this.formProject.addControl('pesoCriterio' + index, new FormControl('')); // Campo de entrada numérica
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    // Lógica de submissão aqui
+  }
 }
