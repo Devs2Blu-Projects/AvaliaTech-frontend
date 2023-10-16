dc82f2e126c6208661b49d3c8669aacd4422f490
raphael-teodoro <raphaelcteodoro@hotmail.com>
2023-10-16 20:23:40 -0300

    fix: Presentation errors.

--- evaluator/src/app/components/admin/presentation/dialog-presentation/dialog-presentation.component.ts
+++ evaluator/src/app/components/admin/presentation/dialog-presentation/dialog-presentation.component.ts
@@ -3,7 +3,6 @@ import { EventModel } from '../../../../shared/interfaces';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { HttpService } from '../../../../shared/services/http/http.service';
 import { UpdateService } from '../../../../shared/services/update/update.service';
-import { ToastComponent } from '../../../../shared/components/toast/toast.component';
 import { DialogTeamComponent } from '../../team/dialog-team/dialog-team.component';
 
 @Component({
@@ -17,23 +16,19 @@ export class DialogPresentationComponent implements OnInit {
 
   constructor(private _httpService: HttpService, private _updateService: UpdateService, private _dialogRef: MatDialogRef<DialogTeamComponent>,@Inject(MAT_DIALOG_DATA) private _data: { action: string, event: EventModel }) { }
 
-  @ViewChild(ToastComponent) toast!: ToastComponent;
-
   ngOnInit(): void {
 
   }
 
   closeEvent(event: EventModel) {
     this.event.isClosed = true;
-    this._httpService.put('global/currentEvent/close', this.event)
+    this._httpService.put('global/currentEvent/close', this.event, { responseType: 'text' })
       .subscribe({
         next: () => {
           this._updateService.notify('Evento fechado com sucesso!');
-          this.toast.showToast();
         },
         error: (error: any) => {
           this._updateService.notify('Erro ao fechar evento.');
-          this.toast.showToast();
           console.error(error);
         }
       })
@@ -43,15 +38,13 @@ export class DialogPresentationComponent implements OnInit {
 
   publishEvent(event: EventModel) {
     this.event.isPublic = true;
-    this._httpService.put('global/currentEvent/public', this.event)
+    this._httpService.put('global/currentEvent/public', this.event, { responseType: 'text' })
       .subscribe({
         next: () => {
           this._updateService.notify('Evento publicado com sucesso!');
-          this.toast.showToast();
         },
         error: (error: any) => {
           this._updateService.notify('Erro ao publicar evento.');
-          this.toast.showToast();
           console.error(error);
         }
       })
