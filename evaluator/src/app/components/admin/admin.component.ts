1ffc2ad1f3ce12372946e304ef0abe0f24b75c03
Rafael-RD <rafaelreinertd@hotmail.com>
2023-10-16 20:03:53 -0300

    feat: select event

--- evaluator/src/app/components/admin/admin.component.ts
+++ evaluator/src/app/components/admin/admin.component.ts
@@ -12,8 +12,9 @@ import { ToastComponent } from 'src/app/shared/components/toast/toast.component'
 })
 export class AdminComponent implements OnInit, OnDestroy {
   data: any = [];
+  selectedData: any = "";
   updateRef!: Subscription;
-  errorMsg: string = 'Erro ao carregar eventos.';
+  notifications: string[] = ['Erro ao carregar eventos.', 'Erro ao carregar evento selecionado.'];
 
   constructor(private _httpService: HttpService, private _updateService: UpdateService, private _logoutService: LogoutService) { }
 
@@ -23,12 +24,12 @@ export class AdminComponent implements OnInit, OnDestroy {
     this.getAll();
     this.updateRef = this._updateService.update
       .subscribe({
-        next: (response: any) => {
+        next: (response: string) => {
           this.toast.elapsedTime = this._updateService.getElapsedTime();
           this.toast.notification = this._updateService.getNotification();
           this.toast.showToast();
 
-          if (response !== this.errorMsg) this.getAll();
+          // if (this.notifications.every(notification => notification !== response)) this.getAll();
         }
       });
   }
@@ -38,12 +39,42 @@ export class AdminComponent implements OnInit, OnDestroy {
   getAll(): void {
     this._httpService.getAll('event', { responseType: 'json' })
       .subscribe({
-        next: (response: any) => { this.data = response; },
+        next: (response: any) => { this.data = response; this.getSelectedEvent();},
         error: (error: any) => {
-          this._updateService.notify(this.errorMsg);
+          this._updateService.notify(this.notifications[0]);
           console.error(error);
         }
       });
+  }
+
+  getSelectedEvent(): void {
+    this._updateService.startTimer();
+    if (true) {
+      this._httpService.getAll('global/currentEvent', { responseType: 'json' })
+      .subscribe({
+        next: (response: any) => {
+          this.selectedData = response.id;
+        },
+        error: (error: any) => {
+          this._updateService.notify(this.notifications[1]);
+          console.error(error);
+        }
+      });
+    }
+  }
+
+  putSelectedEvent(eventId: number): void {
+    this._updateService.startTimer();
+    this._httpService.putById('global/currentEvent', eventId, {}, { responseType: 'text' })
+      .subscribe({
+        next: (response: any) => {
+          this._updateService.notify('Evento alterado com sucesso.', true);
+        },
+        error: (error: any) => {
+          this._updateService.notify('Erro ao alterar evento.');
+          console.error(error);
+        }
+      })
   }
 
   logout(): void { this._logoutService.logout(); }
